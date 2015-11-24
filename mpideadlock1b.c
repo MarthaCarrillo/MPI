cb61242d36da193d1e4f16362f379b6d91bdd756
MarthaCarrillo <ingmarthacarrillo@gmail.com>
2015-11-24 15:58:45 -0500

    Update mpideadlock1b.c

--- mpideadlock1b.c
+++ mpideadlock1b.c
@@ -12,12 +12,12 @@ int main(int argc,char *argv[]){
 	MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
 
 	if (my_rank == 0) {
-	  MPI_Send(sendbuf, count, MPI_INT, 1, tag, MPI_COMM_WORLD);
-	  MPI_Recv(recvbuf, count, MPI_INT, 1, tag, MPI_COMM_WORLD, &status);
+	  MPI_Send(&sendbuf, count, MPI_INT, 1, tag, MPI_COMM_WORLD);
+	  MPI_Recv(&recvbuf, count, MPI_INT, 1, tag, MPI_COMM_WORLD, &status);
 	}
 	else if (my_rank == 1) {
-	  MPI_Send(sendbuf, count, MPI_INT, 0, tag, MPI_COMM_WORLD);
-	  MPI_Recv(recvbuf, count, MPI_INT, 0, tag, MPI_COMM_WORLD, &status);
+	  MPI_Send(&sendbuf, count, MPI_INT, 0, tag, MPI_COMM_WORLD);
+	  MPI_Recv(&recvbuf, count, MPI_INT, 0, tag, MPI_COMM_WORLD, &status);
 	 }
 
 	MPI_Finalize();
