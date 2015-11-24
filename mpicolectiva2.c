44fd6c3478d5262449565ee797fd15dcff235f13
MarthaCarrillo <ingmarthacarrillo@gmail.com>
2015-11-24 16:07:03 -0500

    Update mpicolectiva2.c

--- mpicolectiva2.c
+++ mpicolectiva2.c
@@ -1,1 +1,34 @@
+#include <stdio.h>
+#include <mpi.h>
 
+int main(int argc, char *argv[]){
+    int i,rank,size;
+    int root,count;
+    int buffer[5];
+    int sumTotal =0;
+    MPI_Status status;
+    MPI_Request request;
+
+    MPI_Init(&argc,&argv);
+    MPI_Comm_size(MPI_COMM_WORLD,&size);
+    MPI_Comm_rank(MPI_COMM_WORLD,&rank);
+    root=0;
+    count=5;
+    if(rank== root){
+      for(i=0;i<count;i++){
+        buffer[i]=i;
+      }
+    }
+
+    // insert your code here
+   if(rank==root){
+      MPI_Reduce(&buffer,&sumTotal, count, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+   }
+
+    printf("Rank is: %d, Value at buffer[%d] is: %d\n, the sum is: %d ",rank, count-1, buffer[count-1],sumTotal);
+    
+    
+    printf("\n");
+    MPI_Finalize();
+    return 0;
+}
