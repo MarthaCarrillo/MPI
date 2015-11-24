06176ab8f624a6179b7cdfe9d7cdc1655314b551
MarthaCarrillo <ingmarthacarrillo@gmail.com>
2015-11-24 15:59:24 -0500

    Update mpideadlock2a.c

--- mpideadlock2a.c
+++ mpideadlock2a.c
@@ -12,10 +12,10 @@ int main(int argc,char *argv[]){
 	MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
 
 	if (my_rank == 0) {
-	  MPI_Sendrecv(sendbuf, count, MPI_INT, 1, tag, recvbuf, count, MPI_INT, 1, tag, MPI_COMM_WORLD, &status);
+	  MPI_Sendrecv(&sendbuf, count, MPI_INT, 1, tag, &recvbuf, count, MPI_INT, 1, tag, MPI_COMM_WORLD, &status);
 	}
 	else if (my_rank == 1) {
-    	  MPI_Sendrecv(sendbuf, count, MPI_INT, 0, tag, recvbuf, count, MPI_INT, 0, tag, MPI_COMM_WORLD, &status);
+    	  MPI_Sendrecv(&sendbuf, count, MPI_INT, 0, tag, &recvbuf, count, MPI_INT, 0, tag, MPI_COMM_WORLD, &status);
 	}
 	MPI_Finalize();
 }
