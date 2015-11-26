97dad7887e67b650ed0f914905488739507df768
MarthaCarrillo <ingmarthacarrillo@gmail.com>
2015-11-26 12:26:01 -0500

    Update mpideadlock2a.c

--- mpideadlock2a.c
+++ mpideadlock2a.c
@@ -2,22 +2,23 @@
 #include "mpi.h"
 
 int main(int argc,char *argv[]){
-	int my_size, my_rank, count=1, tag=1;
-	int recvbuf, sendbuf = 1;
-	MPI_Status status;
-	
+        int my_size, my_rank, count=1, tag=1;
+        int recvbuf, sendbuf = 5;
+        MPI_Status status;
 
-	MPI_Init(&argc,&argv);
-	MPI_Comm_size(MPI_COMM_WORLD, &my_size);
-	MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
 
-	if (my_rank == 0) {
-	  MPI_Sendrecv(&sendbuf, count, MPI_INT, 1, tag, &recvbuf, count, MPI_INT, 1, tag, MPI_COMM_WORLD, &status);
-	  printf("\nHello MPI Message received from process %d: %c, process %d!", 1, recvbuf, my_rank);
-	}
-	else if (my_rank == 1) {
-    	  MPI_Sendrecv(&sendbuf, count, MPI_INT, 0, tag, &recvbuf, count, MPI_INT, 0, tag, MPI_COMM_WORLD, &status);
-    	  printf("\nHello MPI Message received from process %d: %c, process %d!", 0, recvbuf, my_rank);
-	}
-	MPI_Finalize();
+        MPI_Init(&argc,&argv);
+        MPI_Comm_size(MPI_COMM_WORLD, &my_size);
+        MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+
+        if (my_rank == 0) {
+          MPI_Sendrecv(&sendbuf, count, MPI_INT, 1, tag, &recvbuf, count, MPI_INT, 1, tag, MPI_COMM_WORLD, &status);
+          printf("\nHello MPI Message received from process %d: %d, process %d!\n", 1, recvbuf, my_rank);
+        }
+        else if (my_rank == 1) {
+          MPI_Sendrecv(&sendbuf, count, MPI_INT, 0, tag, &recvbuf, count, MPI_INT, 0, tag, MPI_COMM_WORLD, &status);
+          printf("\nHello MPI Message received from process %d: %d, process %d!", 0, recvbuf, my_rank);
+        }
+        MPI_Finalize();
 }
+
