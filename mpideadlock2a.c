27590f5d3dfc47172f0c67f1e84eaa266d496e29
MarthaCarrillo <ingmarthacarrillo@gmail.com>
2015-11-26 12:21:31 -0500

    Update mpideadlock2a.c

--- mpideadlock2a.c
+++ mpideadlock2a.c
@@ -13,9 +13,11 @@ int main(int argc,char *argv[]){
 
 	if (my_rank == 0) {
 	  MPI_Sendrecv(&sendbuf, count, MPI_INT, 1, tag, &recvbuf, count, MPI_INT, 1, tag, MPI_COMM_WORLD, &status);
+	  printf("\nHello MPI Message received from process %d: %c, process %d!", 1, recvbuf, my_rank);
 	}
 	else if (my_rank == 1) {
     	  MPI_Sendrecv(&sendbuf, count, MPI_INT, 0, tag, &recvbuf, count, MPI_INT, 0, tag, MPI_COMM_WORLD, &status);
+    	  printf("\nHello MPI Message received from process %d: %c, process %d!", 0, recvbuf, my_rank);
 	}
 	MPI_Finalize();
 }
