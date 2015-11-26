c79623d4281982c61b135246cde06ee14abc9a18
MarthaCarrillo <ingmarthacarrillo@gmail.com>
2015-11-26 11:08:44 -0500

    Update mpicolectiva1.c

--- mpicolectiva1.c
+++ mpicolectiva1.c
@@ -2,7 +2,8 @@
 #include "mpi.h"
 
 int main(int argc,char *argv[]){
-	int i, sum, sumTotal, upToVal, prodTotal;
+	int i, sum, sumTotal, upToVal;
+	long int prodTotal;
 	int start, end, size, rank;
 
 	upToVal = 10000;
@@ -24,18 +25,12 @@ int main(int argc,char *argv[]){
     		sum = sum +i;
     	}
     	// insert your code here
-    	if(rank==0){
-    	  MPI_Reduce(&sum,&sumTotal, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
-    	  
-    	}
-    	else if(rank==(size-1)){
-    	  MPI_Reduce(&sum,&prodTotal,1, MPI_INT, MPI.PROD, 0, MPI_COMM_WORLD);
-    	}
-
+     	MPI_Reduce(&sum,&sumTotal, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+    	MPI_Reduce(&sum,&prodTotal,1, MPI_LONG, MPI_PROD, size-1, MPI_COMM_WORLD);
+    
     	printf("\nRank: %d, sum: %d, sumTotal: %d, prodTotal: %d, start: %d, end: %d\n", rank, sum, sumTotal, prodTotal, start, end);
-    	
-	
-	MPI_Finalize();
+    
+    	MPI_Finalize();
 	
 	return 0;
 }
